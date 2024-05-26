f828e1ea9b9aadc961dae4d185d484c769a1f045
AdakHaddad <muhammadmuqtada@gmail.com>
2024-05-26 23:59:48 +0700

    flask env

--- src/components/Quran.tsx
+++ src/components/Quran.tsx
@@ -3,7 +3,7 @@ import { FC } from "react";
 const QuranVerse: FC = () => {
   return (
     <div className="text-center my-8 lg:text-xl text-md  ">
-      <p className="italic px-24">
+      <p className="italic lg:px-24 px-4">
         &quot;Sesungguhnya Allah dan malaikat-malaikat-Nya bershalawat untuk
         Nabi. Hai orang-orang yang beriman, bershalawatlah kamu untuk Nabi dan
         ucapkanlah salam penghormatan kepadanya.&quot;
